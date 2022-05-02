966d2bb382bfe0473c2a44ecde665960c090365d
johnmiicheal <michealelijah301@gmail.com>
2022-05-01 18:57:02 -0700

    signup page dev

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,10 +1,14 @@
-import { Flex, Image } from '@chakra-ui/react';
+import { Flex, Heading, Box, Button } from '@chakra-ui/react';
 
 const ImgPage = () =>  (
   <Flex height= "100vh" alignItems= "center" justifyContent= "left">
     <Flex direction="column" background="white" p={12} rounded={10}>
-      <Image src='../assets/signup_img.png' width={100} height={100} />
+      <img src='/signup.png' width={500} height={100} />
     </Flex>
+    <Box maxW = '32rem'>
+      <Heading mb={4}>Sign up to zcamp</Heading>
+      <Button size='lg' colorScheme='green' mt='24px'>Sign up with Google </Button>
+    </Box>
   </Flex>
 )
 
