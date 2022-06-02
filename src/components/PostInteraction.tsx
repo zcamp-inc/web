dc4a1c8ea717f84f2601b70f814a71ecd824b36f
johnmiicheal <michealelijah301@gmail.com>
2022-06-02 21:47:31 +0100

    more colors :)

--- src/components/PostInteraction.tsx
+++ src/components/PostInteraction.tsx
@@ -10,88 +10,81 @@ import { IoChatbubbleOutline } from "react-icons/io5";
 
 export default function PostInteraction() {
   return (
-    <HStack spacing={{ base: 40, md: 60}} >
-      <HStack spacing={{ base: 0, md: 20}}>
-      <Flex
-        align="center"
-        ml={{ base: 2, md: 8}}
-        borderRadius="lg"
-        role="group"
-        color="#000a16"
-        _hover={{ color: "#5E00AB" }}
-      >
-        <IconButton
-          icon={<FiChevronUp />}
-          aria-label="upvote"
-          _hover={{ color: "#5E00AB",  bg: "#DDB2FF" }}
-          fontSize={{ base: 24, md: 26 }}
-          variant="ghost"
-        />
+    <HStack spacing={{ base: 40, md: 60 }}>
+      <HStack spacing={{ base: 0, md: 20 }}>
+        <Flex
+          align="center"
+          ml={{ base: 2, md: 8 }}
+          borderRadius="lg"
+          role="group"
+          color="#000a16"
+          _hover={{ color: "#5E00AB" }}
+        >
+          <IconButton
+            icon={<FiChevronUp />}
+            aria-label="upvote"
+            _hover={{ color: "#5E00AB", bg: "#DDB2FF" }}
+            fontSize={{ base: 24, md: 26 }}
+            variant="ghost"
+          />
 
-        <Text>
-          100
-        </Text>
+          <Text>100</Text>
 
-        <IconButton
-          icon={<FiChevronDown />}
-          aria-label="downvote"
-          fontSize={{ base: 24, md: 26 }}
-          _hover={{ color: "#5E00AB", bg: "#DDB2FF" }}
-          variant="ghost"
-          // mr={10}
-        />
-      </Flex>
+          <IconButton
+            icon={<FiChevronDown />}
+            aria-label="downvote"
+            fontSize={{ base: 24, md: 26 }}
+            _hover={{ color: "#5E00AB", bg: "#DDB2FF" }}
+            variant="ghost"
+            // mr={10}
+          />
+        </Flex>
 
-      <Flex
-        align="center"
-        // p={2}
-        // mx={4}
-        borderRadius="lg"
-        role="group"
-        color="#000a16"
-        _hover={{
-          color: "#5E00AB",
-        }}
-      >
-        <IconButton
-          icon={<IoChatbubbleOutline />}
-          fontSize={{ base: 24, md: 26 }}
-          _groupHover={{ color: "#5E00AB", bg: "#DDB2FF" }}
-          aria-label="comment"
-          variant="ghost"
-         
-        />
-        <Text ml="5px" mr={-20}>10</Text>
-      </Flex>
-
+        <Flex
+          align="center"
+          // p={2}
+          // mx={4}
+          borderRadius="lg"
+          role="group"
+          color="#000a16"
+          _hover={{
+            color: "#487D8D",
+          }}
+        >
+          <IconButton
+            icon={<IoChatbubbleOutline />}
+            fontSize={{ base: 24, md: 26 }}
+            _groupHover={{ color: "#487D8D", bg: "#D3F5FF" }}
+            aria-label="comment"
+            variant="ghost"
+          />
+          <Text ml="5px" mr={-20}>
+            10
+          </Text>
+        </Flex>
       </HStack>
 
-
-        
       <HStack spacing={0}>
-
         <IconButton
-          icon={<FiUpload strokeWidth={1.4}/>}
+          icon={<FiUpload strokeWidth={1.4} />}
           aria-label="share post"
-          variant='ghost'
-          ml={{ base: -10, md: -20}}
-          mr={{ base: 7, md: 20}}
+          variant="ghost"
+          ml={{ base: -10, md: -20 }}
+          mr={{ base: 7, md: 20 }}
           fontSize={{ base: 20, md: 26 }}
-          color='#000a16'
-          _hover={{ color: "#5E00AB", bg: "#DDB2FF" }}
+          color="#000a16"
+          _hover={{ color: "#00A023", bg: "#9FFBB3" }}
         />
- 
-        <IconButton
-          icon={<FiBookmark strokeWidth={1.4}/>}
-          aria-label="save post"
-          variant='ghost'
-          fontSize={{ base: 20, md: 26 }}
-       
-          color='#000a16'
-          _hover={{  bg: "#DDB2FF", color: "#5E00AB" }}
-        />
-        </HStack>
 
+        <IconButton
+          icon={<FiBookmark strokeWidth={1.4} />}
+          aria-label="save post"
+          variant="ghost"
+          fontSize={{ base: 20, md: 26 }}
+          color="#000a16"
+          _hover={{ bg: "#FFC3D9", color: "#FF377F" }}
+        />
+      </HStack>
     </HStack>
   );
 }
